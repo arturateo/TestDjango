af37c642995d41ae5eeebd60a013b5b36a7638c9
Artur Sheshukov <arti.shishka11@gmail.com>
2022-11-06 01:30:14 +0600

    Added view and template for detailed blog post as well as CSS for the site.

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index0'),
+    path('', views.index, name='index'),
+    path('post/<int:pk>/', views.tasks_detail, name='post_detail'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
+from django.utils import timezone
+from .models import Post
 
 def index(request):
-    return render(request, 'tasks/tasks_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'tasks/tasks_list.html', {'posts': posts})
+
+def tasks_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'tasks/post_detail.html', {'post': post})
